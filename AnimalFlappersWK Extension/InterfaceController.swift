fc8450b87d9bcf2fd396f681bb1ad563d3f2ff62
Dylan McCowan <DMAN6194@gmail.com>
2018-11-15 16:13:21 -0500

    Created Project Structure
    Created the AnimalFlappers base SpriteKit project along with the Target
    for Apple WatchKit

---
+++ AnimalFlappersWK Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  AnimalFlappersWK Extension
+//
+//  Created by Dylan McCowan on 2018-11-15.
+//  Copyright © 2018 GreyCodeGroup. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
